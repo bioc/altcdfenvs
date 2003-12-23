40e6a3e2fb8b3d8acfe9022f643ef2ab1671d962
dfcimm2 <unknown>
2003-12-23 17:04:10 +0000

    A package to build alternative cdfenvs (for Affymetrix chips). 	-- LG
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/altcdfenvs@7085 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/cdfenvs.R
@@ -0,0 +1,68 @@
+wrapCdfEnv <- function(cdfenv, nrow.chip, ncol.chip, chiptype) {
+  class(cdfenv) <- c("environment", "CdfEnv")
+  attr(cdfenv, "nrow") <- nrow.chip
+  attr(cdfenv, "ncol") <- ncol.chip
+  attr(cdfenv, "chipType") <- chiptype
+  return(cdfenv)
+}
+
+getCdfEnv <- function(abatch) {
+  if (class(abatch) != "AffyBatch")
+    stop("arg must be of class 'AffyBatch'.")
+
+  cdfenv <- getCdfInfo(abatch)
+  cdfenv <- wrapCdfEnv(cdfenv, abatch@nrow, abatch@ncol, abatch@cdfName)
+  return(cdfenv)
+}
+
+buildCdfEnv <- function(matches, ids, probes.pack, abatch=NULL, nrow.chip=NULL, ncol.chip=NULL, chiptype=NULL, mm=NA) {
+
+  if (! (is.list(matches) && length(matches > 0) && length(matches < 3)))
+    stop("arg 'matches' should be a list a returned by matchprobes.")
+
+  if (length(matches) != length(ids))
+    stop("'matches' and 'ids' must have the same length.")
+
+  if ( ! is.null(abatch)) {
+    if (class(abatch) != "AffyBatch")
+      stop("abatch must be of class 'AffyBatch'.")
+    nrow.chip <- abatch@nrow
+    ncol.chip <- abatch@ncol
+    chiptype <- abatch@cdfName
+  }
+
+  if (is.null(nrow.chip) || is.null(ncol.chip) || is.null(chiptype))
+    stop("nrow.chip, ncol.chip or chiptype not defined.")
+  
+  do.call("library", list(probes.pack))
+  probe.tab <- get(probes.pack, envir=as.environment(paste("package:", probes.pack, sep="")))
+
+  cdfenv <- new.env(hash=TRUE)
+
+  for (i in seq(along=matches$match)) {
+    xy <- getxy.probeseq(probeseq=probe.tab, i.row=matches$match[[i]])
+    assign(ids[i],
+           cbind(xy2indices(xy[, 1], xy[, 2], nr=nrow.chip), mm),
+           envir=cdfenv)
+  }
+  
+  cdfenv <- wrapCdfEnv(cdfenv, nrow.chip, ncol.chip, chiptype)  
+  return(cdfenv)  
+}
+
+getxy.probeseq <- function(ppset.id=NULL, probeseq=NULL, i.row=NULL, offset.one=TRUE) {
+  if (sum(c(is.null(ppset.id), is.null(i.row))) != 1)
+    stop("specify one and only one of 'ppset.id', 'i.row'")
+
+  if (is.null(i.row))
+    i.row <- probeseq$Probe.Set.Name %in% ppset.id
+
+  mm.offset <- rep(0, length=length(i.row))
+  mm.offset[i.row < 0] <- 1
+  i.row <- abs(i.row)
+  
+  xy <- cbind(probeseq$Probe.X[i.row], probeseq$Probe.Y[i.row] + mm.offset) + 1
+  colnames(xy) <- c("x", "y")
+  return(xy)
+}
+
---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.First.lib <- function(libname, pkgname, where) {
+
+  library(Biobase)
+  library(affy)
+  library(matchprobes)
+  library(splicegear)
+  
+#   message <- FALSE
+
+#   if (message) {
+#   }
+
+}
